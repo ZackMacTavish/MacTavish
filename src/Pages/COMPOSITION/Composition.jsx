c4e64431e9c9d8f134052e3b491d8252d8461df1
Zachary MacTavish <zacharymactavish@Zacharys-MacBook-Pro.local>
2021-09-28 12:46:22 -0700

    added full composition page

--- src/Pages/COMPOSITION/Composition.jsx
+++ src/Pages/COMPOSITION/Composition.jsx
@@ -12,6 +12,20 @@ import twin2 from '../../img/Twin2.jpeg';
 import staircases from '../../img/Staircases.jpg';
 import heartwhite from '../../img/heartwhite.jpeg';
 import heartblack from '../../img/heartblack.jpg';
+import birds from '../../img/Birds.jpg';
+import brooklyn from '../../img/Brooklyn.jpg';
+import coney from '../../img/Coney.jpg';
+import brooklyn2 from '../../img/Brooklyn2.jpg';
+import render4 from '../../img/Render4.png';
+import rug from '../../img/Rug.jpg';
+import mash1 from '../../img/Mash1.png';
+import mash2 from '../../img/Mash2.png';
+import render5 from '../../img/Render5.png';
+import mash3 from '../../img/Mash3.png';
+import mash4 from '../../img/Mash4.png';
+import mash5 from '../../img/Mash5.png';
+import mattos from '../../img/Mattos.png';
+import og from '../../img/No.3.png';
 
 const LanderSection = styled.div`
 display: flex;
@@ -26,7 +40,7 @@ const FullCover = styled.img`
 width: 60vw;
 
 `
-
+{/* Art resting on row one */}
 const Orbital = styled.img`
 grid-row-start: 1;
 grid-column-start: 2;
@@ -34,6 +48,16 @@ justify-self: flex-start;
 height: 85vh;
 width: auto;
 `
+{/* Art resting on row one */}
+const Rug = styled.img`
+grid-row-start: 1;
+grid-column-start: 2;
+justify-self: flex-start;
+height: auto;
+width: 40vw;
+align-self: flex-end;
+`
+
 
 const TwinTwo = styled.img`
 grid-row-start: 1;
@@ -53,13 +77,20 @@ margin: 0;
 line-height: 0.6;
 font-family: 'Space Grotesk', sans-serif;
 `
-
+{/* Art resting on row two */}
 const GridRowTwo = styled.img`
 grid-row-start: 3;
 grid-column-start: 2;
 justify-self: flex-start;
 height: 85vh;
-
+width: auto;
+`
+{/* Art resting on row three */}
+const GridRowThree = styled.img`
+grid-row-start: 5;
+grid-column-start: 2;
+justify-self: flex-start;
+height: 85vh;
 width: auto;
 `
 
@@ -77,6 +108,14 @@ font-weight: 500;
 font-size: 2.7rem;
 width: 40vw;
 color: #626262;
+`
+{/* Section with three standard pieces column wise */}
+export const ArtSectionthree = styled.div`
+display: grid;
+grid-template-columns: 10vw 40vw 40vw 10vw;
+grid-template-rows: ${(props) => props.Grids};
+padding-top: 5vh;
+background-color:${(props) => props.Backgroundcolor};
 `
 {/* Section with two standard pieces column wise */}
 export const ArtSection = styled.div`
@@ -102,6 +141,8 @@ grid-template-rows: 85vh 10vh;
 padding-top: 5vh;
 background-color:${(props) => props.Backgroundcolor};
 `
+
+{/* Text sitting on row one*/}
 export const ArtText = styled.div`
 display: grid;
 grid-column-start: 3;
@@ -109,12 +150,12 @@ grid-row-start: 1;
 align-self: flex-end;
 padding: 0;
 margin: 0;
-padding-left: 4vw;
+padding-left: 4.5vw;
 line-height: 0;
 color: #626262;
 font-family: 'Space Grotesk', sans-serif;
 `
-
+{/* Text sitting on row two*/}
 export const ArtTexttwo = styled.div`
 display: grid;
 grid-column-start: 3;
@@ -122,7 +163,20 @@ grid-row-start: 3;
 align-self: flex-end;
 padding: 0;
 margin: 0;
-padding-left: 4vw;
+padding-left: 4.5vw;
+line-height: 0;
+color: #626262;
+font-family: 'Space Grotesk', sans-serif;
+`
+{/* Text sitting on row three*/}
+export const ArtTextthree = styled.div`
+display: grid;
+grid-column-start: 3;
+grid-row-start: 5;
+align-self: flex-end;
+padding: 0;
+margin: 0;
+padding-left: 4.5vw;
 line-height: 0;
 color: #626262;
 font-family: 'Space Grotesk', sans-serif;
@@ -156,6 +210,8 @@ export default function Composition() {
         
                 <FullBg src={renderone} />
 
+
+{/*Art Book + Text */}
                 <NewSection>
                     
 <HouseOne src={artbook} />
@@ -165,6 +221,8 @@ the show.
 
                     </ParagraphFour>
                     </NewSection>
+
+                    {/* Orbital + Erasure I */}
 
                     <ArtSection>
                     <Orbital src={orbital} />
@@ -186,9 +244,6 @@ the show.
 
                             </ArtTexttwo>
                         
-                        
-                    
-
                     </ArtSection>
 
 {/*RENDER 2 FULL SCREEN */}
@@ -231,9 +286,148 @@ the show.
 
                     </TwinsText>
                     </ArtSectiontwo>
-                    
 
-       
+                    {/*BIRDS FULL SCREEN */}
+                    <FullBg src={birds} />
+
+
+                    {/* 3 photos - Passing Grid-template-row props */}
+                    <ArtSectionthree Grids='85vh 7vh 85vh 7vh 85vh 7vh'>
+                    <Orbital src={coney} />
+                            <ArtText> 
+
+                            <ArtHeader>CONEY ISLAND, NY</ArtHeader>
+                            <ArtYear>2021</ArtYear>
+                            <ArtDesc>Film photograph with light leaks.</ArtDesc>
+
+                            </ArtText>
+
+
+                            <GridRowTwo src={brooklyn} />
+                            <ArtTexttwo> 
+
+                            <ArtHeader>BROOKLYN, NY I</ArtHeader>
+                            <ArtYear>2021</ArtYear>
+                            <ArtDesc>Double exposure film photograph.</ArtDesc>
+
+                            </ArtTexttwo>
+
+                            <GridRowThree src={brooklyn2} />
+                            <ArtTextthree> 
+
+                            <ArtHeader>BROOKLYN, NY II</ArtHeader>
+                            <ArtYear>2021</ArtYear>
+                            <ArtDesc>Double exposure film photograph.</ArtDesc>
+
+                            </ArtTextthree>
+
+                            </ArtSectionthree>
+
+                               {/*Render4 FULL SCREEN */}
+                    <FullBg src={render4} />
+
+                    {/* 3 Mashes - Passing Grid-template-row props*/}
+                    <ArtSectionthree Grids='60vh 7vh 85vh 7vh 85vh 7vh'>
+                    <Rug src={rug} />
+                            <ArtText> 
+
+                            <ArtHeader>RUG I</ArtHeader>
+                            <ArtYear>2021</ArtYear>
+                            <ArtDesc>100% Wool & Monks cloth.</ArtDesc>
+
+                            </ArtText>
+
+
+                            <GridRowTwo src={mash1} />
+                            <ArtTexttwo> 
+
+                            <ArtHeader>Design for COMPOSITION III</ArtHeader> 
+                            <ArtYear>2019</ArtYear> 
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc> 
+
+                            </ArtTexttwo>  
+
+                            <GridRowThree src={mash2} />  
+                            <ArtTextthree> 
+
+                            <ArtHeader>Design for COMPOSITION IV</ArtHeader> 
+                            <ArtYear>2019</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc> 
+
+                            </ArtTextthree> 
+
+                            </ArtSectionthree>
+
+                                {/*Render5 FULL SCREEN */}
+                    <FullBg src={render5} />
+
+                     {/* 3 photos - Passing Grid-template-row props */}
+                     <ArtSectionthree Grids='85vh 7vh 85vh 7vh 85vh 7vh'>
+                    <Orbital src={mash3} />
+                            <ArtText> 
+
+                            <ArtHeader>Design for COMPOSITION V</ArtHeader>
+                            <ArtYear>2019</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc>
+
+                            </ArtText>
+
+
+                            <GridRowTwo src={mash4} />
+                            <ArtTexttwo> 
+
+                            <ArtHeader>Design for COMPOSITION VI</ArtHeader>
+                            <ArtYear>2019</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc>
+
+                            </ArtTexttwo>
+
+                            <GridRowThree src={mash5} />
+                            <ArtTextthree> 
+
+                            <ArtHeader>Design for COMPOSITION VII</ArtHeader>
+                            <ArtYear>2020</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc>
+
+                            </ArtTextthree>
+
+                            </ArtSectionthree>
+
+                                  {/*MATTOS FULL SCREEN */}
+                                    <FullBg src={mattos} />
+
+                        {/* 3 Pieces - Passing Grid-template-row props */}
+                     <ArtSectionthree Grids='85vh 7vh 85vh 7vh 85vh 7vh'>
+                    <Orbital src={og} />
+                            <ArtText> 
+
+                            <ArtHeader>Design for COMPOSITION I</ArtHeader>
+                            <ArtYear>2018</ArtYear>
+                            <ArtDesc>Digital collage (study for a screenprint).</ArtDesc>
+
+                            </ArtText>
+
+
+                            <GridRowTwo src={mash4} />
+                            <ArtTexttwo> 
+
+                            <ArtHeader>Design for COMPOSITION VI</ArtHeader>
+                            <ArtYear>2019</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc>
+
+                            </ArtTexttwo>
+
+                            <GridRowThree src={mash5} />
+                            <ArtTextthree> 
+
+                            <ArtHeader>Design for COMPOSITION VII</ArtHeader>
+                            <ArtYear>2020</ArtYear>
+                            <ArtDesc>Digital collage (study for a painting).</ArtDesc>
+
+                            </ArtTextthree>
+
+                            </ArtSectionthree>
+
 
 
     </div>
